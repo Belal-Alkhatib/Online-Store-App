73e9b36024b78f3f8ceb30ff1b92f16afc631420
cs310 <cs310.se@gmail.com>
2022-04-30 01:53:14 +0300

    Moomen is coming

---
+++ app/src/main/java/com/example/and2_finalproject/Splash.kt
@@ -0,0 +1,25 @@
+package com.example.and2_finalproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //        hide status par
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+//            hide action par
+        supportActionBar?.hide()
+        Handler().postDelayed({val i = Intent(this,ShowDetailsShop::class.java)
+            startActivity(i)
+            finish()},3000)
+    }
+}
