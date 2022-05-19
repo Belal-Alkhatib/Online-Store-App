aa4b5aea7abf4a7daaa7162c0a8df0637552d9e2
cs310 <cs310.se@gmail.com>
2022-05-19 14:32:24 +0300

    abb bar

--- app/src/main/java/com/example/and2_finalproject/MainActivity.kt
+++ app/src/main/java/com/example/and2_finalproject/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.and2_finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.and2_finalproject.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
@@ -11,9 +15,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val bottomNav = binding.bottomNavigationView
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment,R.id.searchFragment,R.id.profileFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottomNav.setupWithNavController(navController)
 
     }
 }
